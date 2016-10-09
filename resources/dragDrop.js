210a926725c563b4a7100dc32730534b379a8ace
thatguythere1010 <sammyjster192@gmail.com>
2016-10-09 17:36:36 +0100

    Userscript Interfacing Update
    Handling DOM page-side

--- resources/dragDrop.js
+++ resources/dragDrop.js
@@ -10,31 +10,30 @@ function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   var id = $("steamModDrop").text();
-  alert(id);
-            var text = $(id).text();
-            var colour = prompt("Please enter a colour (green, yellow, red)");
-            var valid = false;
-            switch (colour) {
-                case "Green":
-                    colour = "rgba(0, 255, 0, 0.2)";
-                    valid = true;
-                    break;
-                case "Yellow":
-                    colour = "rgba(255, 255, 0, 0.2)";
-                    valid = true;
-                    break;
-                case "Red":
-                    colour = "rgba(255, 0, 0, 0.2)";
-                    valid = true;
-                    break;
-                default:
-                    valid = false;
-                    break;
-            }
+  var text = $(id).text();
+  var colour = prompt("Please enter a colour (green, yellow, red)");
+  var valid = false;
+  switch (colour) {
+  case "Green":
+    colour = "rgba(0, 255, 0, 0.2)";
+    valid = true;
+    break;
+  case "Yellow":
+    colour = "rgba(255, 255, 0, 0.2)";
+    valid = true;
+    break;
+  case "Red":
+    colour = "rgba(255, 0, 0, 0.2)";
+    valid = true;
+    break;
+  default:
+    valid = false;
+    break;
+  }
 
-            if (valid) {
-                $("#steamModDropButton").html(text + "," + colour).click();
-            } else {
-                alert("Invalid Colour!");
-            }
+  if (valid) {
+    $("#steamModDropButton").html(text + "," + colour).click();
+  } else {
+    alert("Invalid Colour!");
+  }
 }
